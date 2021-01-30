2a5bf19893a2a484b2ff062c268a560be4eaaaee
beatyp <paul.beaty@stjude.org>
2021-01-30 13:18:36 -0600

    remove calls to react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import * as BABYLON from "@babylonjs/core";
 import "@babylonjs/core/Debug/debugLayer"; // Augments the scene with the debug methods
 import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
